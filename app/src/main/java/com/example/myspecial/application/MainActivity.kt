926214748a91a2249f811ae12c15b1f879839b9c
amarnathbaitha <amarnathbaitha@gmail.com>
2025-06-16 14:42:55 +0530

    Toast example

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myspecial.application
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,12 +11,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,9 +44,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
+
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier.height(400.dp).width(220.dp)
     ) {
         Text(
@@ -54,6 +58,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Having fun",
 
             )
+        val context = LocalContext.current
+        Button(onClick = {
+
+            Toast.makeText(context,"Toast checking",Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Click Me")
+        }
     }
 
 }
