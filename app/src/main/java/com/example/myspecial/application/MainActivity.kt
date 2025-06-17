bfb7524e3c5c9dc1c2b838288d9fd3c4c44b4254
amarnathbaitha <amarnathbaitha@gmail.com>
2025-06-18 01:02:56 +0530

    image resource

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.example.myspecial.application
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.ui.theme.AppTheme
 import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
 
@@ -30,7 +34,6 @@ class MainActivity : ComponentActivity() {
             MySpecialApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Amar",
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
@@ -40,28 +43,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
 
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.bodySmall
+        Image(
+            painter = painterResource(R.drawable.logo),
+            modifier = Modifier.size(256.dp).clip(CircleShape).background(Color.Magenta).padding(16.dp),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
         )
-        Text(
-            text = "Having fun",
 
-            )
-        val context = LocalContext.current
-        Button(onClick = {
-
-            Toast.makeText(context,"Toast checking",Toast.LENGTH_SHORT).show()
-        }) {
-            Text("Click Me")
-        }
     }
 
 }
@@ -77,7 +72,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 )
 @Composable
 fun GreetingPreview() {
-    AppTheme  {
-        Greeting("Android")
+    AppTheme {
+        Greeting()
     }
 }
