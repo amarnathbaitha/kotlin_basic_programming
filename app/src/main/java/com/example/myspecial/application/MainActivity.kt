549faac33331469da89aecd062773890168aaad4
amarnathbaitha <amarnathbaitha@gmail.com>
2025-06-18 01:52:13 +0530

    resouce switches with composed

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -5,20 +5,34 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,27 +58,28 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
+    var checked by remember { mutableStateOf(true) }
+    val resourceId = if(checked) R.drawable.logo else R.drawable.baby
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.height(400.dp).width(220.dp)
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Hello $name!",
-            style = MaterialTheme.typography.bodySmall
+        Switch(
+            onCheckedChange = { checked = it },
+            checked = checked,
+
         )
-        Text(
-            text = "Having fun",
-
-            )
-        val context = LocalContext.current
-        Button(onClick = {
-
-            Toast.makeText(context,"Toast checking",Toast.LENGTH_SHORT).show()
-        }) {
-            Text("Click Me")
-        }
+       Image(
+           painter = painterResource(resourceId),
+           contentDescription = null,
+           contentScale = ContentScale.Crop,
+           modifier = Modifier
+               .size(256.dp)
+               .clip(CircleShape)
+               .background(Color.Green)
+               .padding(16.dp))
     }
 
 }
